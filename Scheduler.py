ad2f51ac5f37226d6846ea4b59dc7c06e211a56d
coderpiaobozhe <63344459+coderpiaobozhe@users.noreply.github.com>
2023-03-06 15:15:22 +0800

    Update Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -1,12 +1,12 @@
 from torch.optim.lr_scheduler import _LRScheduler
 
 class GradualWarmupScheduler(_LRScheduler):
-    def __init__(self, optimizer, multiplier, warm_epoch, after_scheduler=None):
+    def __init__(self, optimizer, multiplier, warm_epoch, after_scheduler = None, last_epoch = None):
         self.multiplier = multiplier
         self.total_epoch = warm_epoch
         self.after_scheduler = after_scheduler
         self.finished = False
-        self.last_epoch = None
+        self.last_epoch = last_epoch
         self.base_lrs = None
         super().__init__(optimizer)
 
@@ -19,7 +19,13 @@ class GradualWarmupScheduler(_LRScheduler):
                 return self.after_scheduler.get_last_lr()
             return [base_lr * self.multiplier for base_lr in self.base_lrs]
         return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
-
+    def state_dict(self):
+        warmdict = {key:value for key, value in self.__dict__.items() if (key != 'optimizer' and key != 'after_scheduler')}
+        cosdict = {key:value for key, value in self.after_scheduler.__dict__.items() if key != 'optimizer'}
+        return {'warmup':warmdict, 'afterscheduler':cosdict}
+    def load_state_dict(self, state_dict: dict):
+        self.after_scheduler.__dict__.update(state_dict['afterscheduler'])
+        self.__dict__.update(state_dict['warmup'])
 
     def step(self, epoch=None, metrics=None):
         if self.finished and self.after_scheduler:
