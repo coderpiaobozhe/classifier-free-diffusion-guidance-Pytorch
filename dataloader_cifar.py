845ddc2fd54a2a8be1203531ab69b72b25a7c5fa
coderpiaobozhe <63344459+coderpiaobozhe@users.noreply.github.com>
2023-03-06 15:15:59 +0800

    Update dataloader_cifar.py

--- dataloader_cifar.py
+++ dataloader_cifar.py
@@ -1,22 +1,29 @@
+from torch import Tensor
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
+from torch.utils.data.distributed import DistributedSampler
 
-def load_data(params):
+def load_data(batchsize:int, numworkers:int) -> tuple[DataLoader, DistributedSampler]:
     trans = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
-    data_train = CIFAR10(root = './',
-                    train = True,
-                    download = False,
-                    transform = trans)
-    trainloader = DataLoader(data_train,
-                            batch_size = params.batchsize,
-                            shuffle = True,
-                            num_workers = params.numworkers,
-                            drop_last = True,
-                            pin_memory = True)
-    return trainloader
-def transback(data):
+    data_train = CIFAR10(
+                        root = './',
+                        train = True,
+                        download = False,
+                        transform = trans
+                    )
+    sampler = DistributedSampler(data_train)
+    trainloader = DataLoader(
+                        data_train,
+                        batch_size = batchsize,
+                        num_workers = numworkers,
+                        sampler = sampler,
+                        drop_last = True
+                    )
+    return trainloader, sampler
+
+def transback(data:Tensor) -> Tensor:
     return data / 2 + 0.5
